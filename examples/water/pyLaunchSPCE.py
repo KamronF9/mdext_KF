b4ff691e2b8ccc5fd6d90ee1389e6faa87c0ee43
Kamron Fazel <kamron@aimp.mat.rpi.edu>
2023-07-25 23:36:54 -0400

    many adjustments for water

---
+++ examples/water/pyLaunchSPCE.py
@@ -0,0 +1,40 @@
+import os
+from unicodedata import decimal
+import numpy as np
+
+# nohup python ../pyLaunchBMH.py > out &
+# xx nohup bash ../nacl_bmh.job > out &
+
+
+# Loop over U in gaussian potential and launch jobs
+
+# s = 1. # sigma width gaussian Ang
+# T = 1300. # kelvin
+# P = 1. # pressue  can be overridden later in nacl_bmh.py to be none TODO clean up
+# p = 2 # atom type to apply the potential to (1-based)
+# g = 'planar'
+
+# sweep through potential amplitudes option:
+# endRange = 5.0
+# stepSize = 0.5
+
+# Ui=-0.4
+
+
+    
+
+# for Ui in np.around(np.arange(0,endRange*2 + stepSize ,stepSize), decimals=1)-endRange:  
+# direct plug values in option
+for Ui in [-2.5, 2.5]:  
+    print(f"{Ui:+.1f}")
+    # o = f"test-U{Ui:+.1f}.h"
+    # log = o[:-3]+"_out"
+
+    os.system(f"sbatch ../spce.job {Ui}")
+    # os.system(f"sbatch ../water_spce.job {Ui} {s} {T} {P} {p} {g} {o}")
+    # os.system(f"bash ../nacl_bmh.job {Ui} {s} {T} {P} {p} {g} {o} > {log} &")  
+    # break
+   
+
+    
+    
--- examples/water/spceNima20JUL23.py
+++ examples/water/spceNima20JUL23.py
@@ -13,7 +13,8 @@ def main() -> None:
     seed = 345
     U0 = (float(sys.argv[1]))*23.06   # Amplitude of the external potential (kcal/mol)
     sigma = 1.0 # Width of the external potential (A)
-    jj=float(sys.argv[1])
+    global jj
+    jj=float(sys.argv[1]) # eV
     # Initialize and run simulation:
     md = mdext.md.MD(
         setup=setup,
@@ -27,7 +28,7 @@ def main() -> None:
         timestep=1,
     )
     md.run(1, "equilibration")
-    # md.reset_stats()
+    md.reset_stats() # was commented to allow dump?
     md.run(2, "collection", f"water-lj-pl-U{jj:+.1f}-{sigma:+.2f}.h5")
 
 
@@ -35,7 +36,8 @@ def setup(lmp: PyLammps, seed: int) -> int:
     """Setup initial atomic configuration and interaction potential."""
     
     # Construct water box:
-    L = np.array([26.44, 24.67, 23.60])  # overall box dimensions
+    # L = np.array([26.44, 24.67, 23.60])  # overall box dimensions
+    L = np.array([26.44/2, 24.67/2, 23.60/2])  # overall box dimensions
     file_liquid = "liquid.data20"
     is_head = (MPI.COMM_WORLD.rank == 0)
     if is_head:
@@ -68,8 +70,10 @@ def setup(lmp: PyLammps, seed: int) -> int:
     # Rigid molecule constraints for dynamics:
     
     lmp.neigh_modify("exclude none")
-    lmp.dump("write all custom 100 20_2.lammpstrj id type x y z vx vy vz")
-    # lmp.fix("BondConstraints all shake 0.001 20 0 b 1 a 1")
+    # lmp.dump("write all custom 100 20_2.lammpstrj id type x y z vx vy vz")
+    # # lmp.fix("BondConstraints all shake 0.001 20 0 b 1 a 1")
+    lmp.dump(f"write all custom 1000 pylammps{jj:+.1f}.dump id type element x y z")
+    lmp.dump_modify("write element H O")
 
 
 if __name__ == "__main__":
