51522422c4d1010c6b7d027db4f1dadf09dfcd3a
KamronF9 <kamron.fazel@outlook.com>
2024-01-02 10:29:19 -0500

    random progress

--- potentialShapes.py
+++ potentialShapes.py
@@ -3,36 +3,39 @@ from dataclasses import dataclass
 import matplotlib.pyplot as plt
 
 @dataclass
-class Random:
-    """Random potential of gaussian with peak `U0` and width `sigma` and 
+class GaussianPolynomial:
+    """potential of gaussian with peak `U0` and width `sigma` and 
     polynomial with `a0-2` in terms of r_sq/sigma_sq
-    Ensure goes to 0 by 3/4 of box or so
     """
     U0: float  #: Strength
     sigma: float  #: width of Gaussian
-    a0: float #: +/- nondimensional polynomial coeff a0
-    a1: float #: +/- nondimensional polynomial coeff a1
-    a2: float #: +/- nondimensional polynomial coeff a2
+    polyCoeffs: list #: list of floats for polynomial coefficients
+    # a0: float #: +/- nondimensional polynomial coeff a0
+    # a1: float #: +/- nondimensional polynomial coeff a1
+    # a2: float #: +/- nondimensional polynomial coeff a2
     
 
     def __call__(self, r_sq: np.ndarray):
         sigma_sq = self.sigma**2
         E1 = np.exp(-0.5*r_sq/sigma_sq)
-        coef = [a0,a1,a2]
-        polyEq = np.polynomial.polynomial.Polynomial(coef, domain=None, window=None)
-        E2 = polyEq(r_sq/sigma_sq)
-        # polyEq.deriv()
         # E2 = a0 + a1*r_sq/sigma_sq + a2*r_sq**2/sigma_sq**2
-        # if coef == np.zeros_like(coef): doesn't work
-        if coef == [0]*len(coef):
+        
+        if polyCoeffs == [0]*len(polyCoeffs):
             # Gaussian only
             E = self.U0 * E1
+            E_norm = np.linalg.norm(E)
+            E *= 1/E_norm
+            r_sq_grad = 1/E_norm # dE/dr_sq
         else:
             # Gaussian and polynomial
+            polyEq = np.polynomial.polynomial.Polynomial(polyCoeffs, domain=None, window=None)
+            E2 = polyEq(r_sq/sigma_sq)
+            polyEqDeriv = polyEq.deriv()(r_sq/sigma_sq)
             E = self.U0 * E1 * E2
-        E_norm = np.linalg.norm(E)
-        E *= 1/E_norm
-        r_sq_grad = 1*1/E_norm # dE/dr_sq
+            E_norm = np.linalg.norm(E)
+            E *= 1/E_norm
+            r_sq_grad = self.U0/E_norm * ( 1/(2*sigma_sq)*E1*E2 + E1*polyEqDeriv )   # dE/dr_sq
+
         return E, r_sq_grad
 
 
@@ -41,18 +44,22 @@ U0=1.  # lambda equivalent to scale all
 sigmaScale = np.random.uniform(10,30)
 # sigma=L/10 # /10 to make sure it goes to zero around half of box by observation
 sigma=L/sigmaScale # /10 to make sure it goes to zero around half of box by observation
-a0,a1,a2 = np.random.uniform(-10.,10.,size=(3))
+# a0,a1,a2 = np.random.uniform(-10.,10.,size=(3))
+polyCoeffs = np.random.uniform(-10.,10.,size=(3)).tolist()
 # a0,a1,a2 = np.zeros(3)
 
 
-test = Random(U0,sigma,a0,a1,a2)
+# test = GaussianPolynomial(U0,sigma,a0,a1,a2)
+test = GaussianPolynomial(U0,sigma,polyCoeffs)
+# test = GaussianPolynomial(U0,sigma,[0.]) # gaussian only
 
 r = np.arange(start=0.,stop=L,step=0.1)
 r_sq = r **2
 E, r_sq_grad = test(r_sq)
 
-plt.plot(r,E)
-
+plt.plot(r,E,label='E')
+plt.plot(r,r_sq_grad,label='grad')
+plt.legend()
 # E_norm = E/np.linalg.norm(E)
 # (E_norm**2).sum() # is 1 OK
 
--- src/mdext/potential.py
+++ src/mdext/potential.py
@@ -13,7 +13,38 @@ Potential = Callable[[np.ndarray], Tuple[np.ndarray, np.ndarray]]
 Note that the derivative is also with respect to the squared-coordinate."""
 
 
+@dataclass
+class Random:
+    """Random potential of gaussian with peak `U0` and width `sigma` and 
+    polynomial with `a0-2` in terms of r_sq/sigma_sq
+    Ensure goes to 0 by 3/4 of box or so
+    """
+    U0: float  #: Strength
+    sigma: float  #: width of Gaussian
+    a0: float #: +/- nondimensional polynomial coeff a0
+    a1: float #: +/- nondimensional polynomial coeff a1
+    a2: float #: +/- nondimensional polynomial coeff a2
+    
 
+    def __call__(self, r_sq: np.ndarray):
+        sigma_sq = self.sigma**2
+        E1 = np.exp(-0.5*r_sq/sigma_sq)
+        coef = [a0,a1,a2]
+        polyEq = np.polynomial.polynomial.Polynomial(coef, domain=None, window=None)
+        E2 = polyEq(r_sq/sigma_sq)
+        # polyEq.deriv()
+        # E2 = a0 + a1*r_sq/sigma_sq + a2*r_sq**2/sigma_sq**2
+        # if coef == np.zeros_like(coef): doesn't work
+        if coef == [0]*len(coef):
+            # Gaussian only
+            E = self.U0 * E1
+        else:
+            # Gaussian and polynomial
+            E = self.U0 * E1 * E2
+        E_norm = np.linalg.norm(E)
+        E *= 1/E_norm
+        r_sq_grad = 1*1/E_norm # dE/dr_sq
+        return E, r_sq_grad
 
 # class Gaussian:
 #     def __init__(self, U0: float, sigma: float) -> None:
