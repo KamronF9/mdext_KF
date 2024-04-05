f908f26ea63f040267f56d243f1c2364f8d84937
KamronF9 <kamron.fazel@outlook.com>
2024-04-05 14:42:10 -0400

    clean up

--- potentialShapesErf.py
+++ potentialShapesErf.py
@@ -1,6 +1,5 @@
 import numpy as np
 from numpy.polynomial.polynomial import Polynomial
-# import scipy
 from dataclasses import dataclass, field
 from typing import Sequence
 import matplotlib.pyplot as plt
@@ -28,7 +27,6 @@ class Error:
     def __call__(self, r_sq: np.ndarray):
         r = np.sqrt(r_sq)
         E = erf(self.sharpness*(r-self.xCenter))*self.scale
-        # plt.plot(x,(erf(sharpness*(x-xCenter))*scale+zOffset))
         if self.complement:
             E = 1-E
         E += self.zOffset
@@ -38,13 +36,6 @@ class Error:
             dE_dx *= -1
         return E, dE_dx
 
-        # x = r_sq/sigma_sq
-        # gauss = np.exp(-0.5*x)
-        # poly =  self.polynomial(x)
-        # dpoly_dx = self.deriv(x)
-        # E = gauss * poly
-        # dE_dx =  gauss * (-0.5 * poly + dpoly_dx)
-        # return E, dE_dx / sigma_sq
 
 
 def main():
@@ -61,9 +52,7 @@ def main():
     zOffset = 0.5
     scale = 0.5
     complement = False
-    # plt.plot(x,(erf(sharpness*(x-xCenter))*scale+zOffset))
-    # plt.plot(x,2/np.sqrt(np.pi)*np.exp(-(sharpness*(x-xCenter))**2))
-    # plt.show()
+
     test = Error(xCenter, sharpness, zOffset, scale, complement)
     
     E, dE_dx = test(x**2)
@@ -83,10 +72,6 @@ def main():
     scale = 0.5
     complement = True
 
-    # plt.plot(x,(1-erf(sharpness*(x-xCenter))*scale+zOffset))
-    # plt.plot(x,-2/np.sqrt(np.pi)*np.exp(-(sharpness*(x-xCenter))**2))
-
-
     test = Error(xCenter, sharpness, zOffset, scale, complement)
     
     E, dE_dx = test(x**2)
@@ -94,58 +79,5 @@ def main():
     plt.plot(x, dE_dx)
     plt.show()
     
-    '''
-
-    # Equation was A exp(-x/2) (1 + Bx)
-    # Algorithm:
-    # Pick B using randn(), would be positive or negative
-    # If B < 0, set A = 1
-    # If B > 1/2, set A = -1
-    # Otherwise, pick A randomly from +/- 1
-    # In mdext, only use lambda >= 0
-    # Picking a random sigma
-    # coeff = [A, A*B]
-
-    L = 10.
-    U0 = 1.  # lambda equivalent to scale all
-    np.random.seed(1)
-    # sigmaScale = np.random.uniform(10,30)
-    # sigma=L/10 # /10 to make sure it goes to zero around half of box by observation
-    # sigma=L/sigmaScale # /10 to make sure it goes to zero around half of box by observation
-    
-    sigma = np.random.uniform(0.5, 3)
-
-    B = np.random.randn() 
-    if B > 0.5:
-        A = -1.
-    elif B < 0.:
-        A = 1.
-    else:
-        A = np.random.choice([1,-1]) 
-
-    coeffs = A*np.array([1, B])
-
-    # TODO lambda only +
-
-
-
-    # test = GaussianPolynomial(U0,sigma)
-    test = Gaussian(U0, sigma, coeffs)
-
-
-    dr = 0.1
-    r = np.arange(start=0.,stop=L,step=dr)
-    r_sq = r **2
-    E, r_sq_grad = test(r_sq)
-    print("Integral(E^2):", ((E**2).sum() - 0.5*E[0]**2) * dr)
-    print("Expected:", sigma * np.sqrt(np.pi) * 0.5) # 1/2 box in
-
-    plt.plot(r,E,label='E')
-    # plt.plot(r,r_sq_grad,label='grad')
-    # plt.plot(r,r_sq_grad*(-2)*r,label='F')
-    plt.xlabel('r')
-    plt.legend()
-    '''
-
 if __name__ == "__main__":
     main()
